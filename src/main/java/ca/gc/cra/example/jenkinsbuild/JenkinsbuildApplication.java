b8441ddb72ea864e536f7a5077902b5ba5c8745c
benjy.goulet@gmail.com <benjy.goulet@gmail.com>
2020-10-19 20:26:23 -0400

    project addition for jenkins to compile some thing

---
+++ src/main/java/ca/gc/cra/example/jenkinsbuild/JenkinsbuildApplication.java
@@ -0,0 +1,13 @@
+package ca.gc.cra.example.jenkinsbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsbuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gc/cra/example/jenkinsbuild/JenkinsbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gc.cra.example.jenkinsbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
